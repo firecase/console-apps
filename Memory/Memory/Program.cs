b8ae38ff6bb48be2816e5ba3516080c634a2ce06
firecase <timohte@gmx.ch>
2022-12-20 00:09:56 +0100

    work on

--- Memory/Memory/Program.cs
+++ Memory/Memory/Program.cs
@@ -1,7 +1,25 @@
 ﻿Random random = new Random();
-
 int[,] board = new int[4, 4];
 
+// create print method
+void printOpenBoard()
+{
+    for (int y = 0; y <= 3; y++)
+    {
+        for (int x = 0; x <= 3; x++)
+        {
+            if (x == 3)
+            {
+                Console.WriteLine(board[x, y] + " ");
+            } else
+            {
+                Console.Write(board[x, y] + " ");
+            }
+        }
+    }
+}
+
+// initialize array
 for (int y = 0; y <= 3; y++)
 {
     for (int x = 0; x <= 3; x++)
@@ -10,7 +28,4 @@ for (int y = 0; y <= 3; y++)
     }
 }
 
-foreach (int i in board)
-{
-    Console.WriteLine(i);
-}
+printOpenBoard();
