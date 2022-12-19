450d9523dc876e7430d4247ef66acc137cc0c82c
firecase <timohte@gmx.ch>
2022-12-19 01:33:10 +0100

    configure memory

---
+++ Memory/Memory/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+
+int[,] board = new int[4, 4];
+
+for (int y = 0; y <= 3; y++)
+{
+    for (int x = 0; x <= 3; x++)
+    {
+        board[y,x] = random.Next(10);
+    }
+}
+
+foreach (int i in board)
+{
+    Console.WriteLine(i);
+}
